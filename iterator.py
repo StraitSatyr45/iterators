144f618dab525f3560e9593dccfaa231183af0c8
Umair <umairahmedroll19@gmail.com>
2022-01-22 11:28:05 +0530

    Initial commit

---
+++ iterator.py
@@ -0,0 +1,33 @@
+#learn iterator in python
+
+#declare a list 
+list1 = [1,2,3,4,5,6,7,8,9,10]
+
+#create a variable to store the iterator
+itr = iter(list1)
+
+#use this command to go to a next element
+print(next(itr))
+print(next(itr))
+print(next(itr))
+print(next(itr))
+print(next(itr))
+print(next(itr))
+print(next(itr))
+print(next(itr))
+print(next(itr))
+print(next(itr))
+
+#use this to reverse the list iterator
+itr = reversed(list1)
+#now continue using this command
+print(next(itr))
+print(next(itr))
+print(next(itr))
+print(next(itr))
+print(next(itr))
+print(next(itr))
+print(next(itr))
+print(next(itr))
+print(next(itr))
+print(next(itr))
